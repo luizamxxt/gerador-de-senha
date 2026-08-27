f32165ec452aecf98f41355136baaa30c1d9e1e3
luizamxxt <barbosa.almeida.maria@escola.pr.gov.br>
2026-08-27 12:39:39 -0300

    SENHA

---
+++ main.js
@@ -0,0 +1,54 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto')
+const campoSenha = document. querySelector('#campo-senha')
+
+campoSenha.value = 1230
+
+tamanhoSenha = 8
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao') 
+ // pega o 1°botao = -
+ botoes[0].onclick = diminuir;
+ // funcao diminuir
+function diminuir(){
+      // diminuir  de 1 em 1 e mostra na tela
+    tamanhoSenha--;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+// BOTAO DE AUMeNTAR 
+// pega o 1°botao = +
+ botoes[1]. onclick = AUMENTAR;
+ // funcao  AUMENTAR(){ 
+ // aumentar de 1 em 1 e mostra na tela
+ tamanhoSenha++;
+ numeroSenha.textContent = tamanhoSenha;
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
