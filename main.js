aadc3e526685c7a497ec2aaeffaedf9eaa50a3a4
luizamxxt <barbosa.almeida.maria@escola.pr.gov.br>
2026-09-03 12:38:33 -0300

    senha

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const campoSenha = document. querySelector('#campo-senha')
 LETRAMAISCULAS
 
 campoSenha.value = 1230
+// OPEN IN WEBVIEM  
+ let LETRAMAISCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ // minusculas, numeros e simbolos
 
 tamanhoSenha = 8
 numeroSenha.textContent = tamanhoSenha;
@@ -16,16 +19,25 @@ function diminuir(){
       // diminuir  de 1 em 1 e mostra na tela
     tamanhoSenha--;
     numeroSenha.textContent = tamanhoSenha;
+    gerasenha()
 }
 
 // BOTAO DE AUMeNTAR 
-// pega o 1°botao = +
- botoes[1]. onclick = AUMENTAR;
- // funcao  AUMENTAR(){ 
- // aumentar de 1 em 1 e mostra na tela
- tamanhoSenha++;
- numeroSenha.textContent = tamanhoSenha;
-}
+
+
+// FUNCAO DE CRIAR A SENHA ALEATÓRIO
+  gerasenha()
+
+  function gerasenha(){
+      let senha= ""
+      // LOOP - repetições
+      for (let i = 0; < tamanhoSenha; i++){
+         let numeroAleatório = Math.random() * 26;
+          numeroAleatório = Math, FLOOR (numeroAleatório)
+          senha = senha + let letrasMAISCULAS[numeroAleatório]
+      }
+         campoSenha.value = senha;
+  }
  
 
 
